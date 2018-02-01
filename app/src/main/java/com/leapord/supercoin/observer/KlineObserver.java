1e123139a0fdfc7fc15aac46072cfaeafc98fcb3
JokerFish <fengzb0216@sina.com>
2018-02-01 11:41:44 +0800

    将分析逻辑放到io线程 优化卡顿

--- app/src/main/java/com/leapord/supercoin/observer/KlineObserver.java
+++ app/src/main/java/com/leapord/supercoin/observer/KlineObserver.java
@@ -18,7 +18,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.leapord.supercoin.R2.id.time;
+import io.reactivex.Observable;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * @author Biao
@@ -51,42 +54,49 @@ public class KlineObserver extends CoinObserver<LiveData> {
     @Override
     public void onNext(LiveData value) {
         Logger.d("处理K线数据");
-        double[] priceFromDepth = Analyzer.getPriceFromDepth(value.getDepth());
-        if (mTradeType == OkCoin.TradeType.T_THORT) {       //短线交易
-            int tendencyByDepth = Analyzer.getDepthTendency(value.getDepth());
-            double[] tendencyByKline = Analyzer.getTendencyByKline(value.getKLineData(), 7);
-            if (SpUtils.getBoolean(Const.AUTO_TRANSACTION, false)) {
-                TradeManager.autoTrade(mSymbol, tendencyByDepth, tendencyByKline, value);
-            } else {
-                Log.i(TAG, "onNext: auto trade closed");
-            }
-            long timeByNearPoint = Analyzer.getPredicateTimeByNearPoint(value.getKLineData(), 5);
+        Observable.create((ObservableOnSubscribe<KlineAnalyzeInfo>) emitter -> {
             KlineAnalyzeInfo klineAnalyzeInfo = new KlineAnalyzeInfo();
-            klineAnalyzeInfo.setCoinName(mSymbol);
-            klineAnalyzeInfo.setTendency(tendencyByDepth);
-            klineAnalyzeInfo.setSellPrice(priceFromDepth[0]);
-            klineAnalyzeInfo.setBuyPrice(priceFromDepth[1]);
-            klineAnalyzeInfo.setTime(timeByNearPoint);
-            EventBus.getDefault().post(klineAnalyzeInfo);
-        } else {        //长线
-            MACDProcessor.process(value.getKLineData());
-            List<Double> dea = MACDProcessor.getDEA();
-            List<Double> dif = MACDProcessor.getDIF();
-            List<Double> macd = MACDProcessor.getMACD();
-            KlineAnalyzeInfo klineAnalyzeInfo = new KlineAnalyzeInfo();
-            klineAnalyzeInfo.setCoinName(mSymbol);
-            klineAnalyzeInfo.setTendency(0);
-            if (SpUtils.getBoolean(Const.AUTO_TRANSACTION, false)) {
-                TradeManager.autoTrade(mSymbol, dif, dea, macd);
-            } else {
-                Log.i(TAG, "onNext: auto trade closed");
+            double[] priceFromDepth = Analyzer.getPriceFromDepth(value.getDepth());
+            if (mTradeType == OkCoin.TradeType.T_THORT) {       //短线交易
+                int tendencyByDepth = Analyzer.getDepthTendency(value.getDepth());
+                double[] tendencyByKline = Analyzer.getTendencyByKline(value.getKLineData(), 7);
+                if (SpUtils.getBoolean(Const.AUTO_TRANSACTION, false)) {
+                    Log.i(TAG, "onNext: auto trade opened");
+                    TradeManager.autoTrade(mSymbol, tendencyByDepth, tendencyByKline, value);
+                } else {
+                    Log.i(TAG, "onNext: auto trade closed");
+                }
+                long timeByNearPoint = Analyzer.getPredicateTimeByNearPoint(value.getKLineData(), 5);
+                klineAnalyzeInfo.setCoinName(mSymbol);
+                klineAnalyzeInfo.setTendency(tendencyByDepth);
+                klineAnalyzeInfo.setSellPrice(priceFromDepth[0]);
+                klineAnalyzeInfo.setBuyPrice(priceFromDepth[1]);
+                klineAnalyzeInfo.setTime(timeByNearPoint);
+            } else {        //长线
+                MACDProcessor.process(value.getKLineData());
+                List<Double> dea = MACDProcessor.getDEA();
+                List<Double> dif = MACDProcessor.getDIF();
+                List<Double> macd = MACDProcessor.getMACD();
+                klineAnalyzeInfo.setCoinName(mSymbol);
+                klineAnalyzeInfo.setTendency(0);
+                if (SpUtils.getBoolean(Const.AUTO_TRANSACTION, false)) {
+                    Log.i(TAG, "onNext: auto trade opened");
+                    TradeManager.autoTrade(mSymbol, dif, dea, macd);
+                } else {
+                    Log.i(TAG, "onNext: auto trade closed");
+                }
+                double[] tendencyByKline = Analyzer.getTendencyByKline(value.getKLineData(), 7);
+                long time = Analyzer.getAutoPredicateTime(value.getKLineData(), (int) tendencyByKline[3]);
+                klineAnalyzeInfo.setSellPrice(priceFromDepth[0]);
+                klineAnalyzeInfo.setBuyPrice(priceFromDepth[1]);
+                klineAnalyzeInfo.setTime(time);
             }
-            double[] tendencyByKline = Analyzer.getTendencyByKline(value.getKLineData(), 7);
-            long time = Analyzer.getAutoPredicateTime(value.getKLineData(), (int) tendencyByKline[3]);
-            klineAnalyzeInfo.setSellPrice(priceFromDepth[0]);
-            klineAnalyzeInfo.setBuyPrice(priceFromDepth[1]);
-            klineAnalyzeInfo.setTime(time);
-            EventBus.getDefault().post(klineAnalyzeInfo);
-        }
+            emitter.onNext(klineAnalyzeInfo);
+        }).subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(info -> EventBus.getDefault().post(info),
+                        throwable -> Log.i(TAG, "calc analyzer error: " + throwable.toString())
+                );
+
     }
 }
