015d42b377ebbfc7081926dfe0812c6d75ff0f83
JokerFish <fengzb0216@sina.com>
2018-04-20 16:31:02 +0800

    新增api

--- app/src/main/java/com/leapord/supercoin/app/OkCoin.java
+++ app/src/main/java/com/leapord/supercoin/app/OkCoin.java
@@ -27,11 +27,14 @@ public interface OkCoin {
 
 
     interface API {
-        //        String API_KEY = "2c31b63a-9d64-4889-935c-25f319102832";
-        String API_KEY = "7752d9e0-d261-491a-8460-49920a13f0ab";
-        //
-//        String SECRET_KEY = "241CFFFF941F14A3320DC5F9AC349955";
-        String SECRET_KEY = "625133937D92A58E9F04BB784A51BC35";
+        //        String API_KEY = "2c31b63a-9d64-4889-935c-25f319102832";  //yuan
+//        String API_KEY = "7752d9e0-d261-491a-8460-49920a13f0ab";          //feng
+
+        String API_KEY = "dc26f54e-8479-4a10-ad44-d39108696f68";        //liu
+
+        //        String SECRET_KEY = "241CFFFF941F14A3320DC5F9AC349955";
+//        String SECRET_KEY = "625133937D92A58E9F04BB784A51BC35";
+        String SECRET_KEY = "6DE251C160C8318B7AAA0FD4E7739E47";
     }
 
     long ONE_PERIOD = 15 * 60 * 1000;
